99c1353b0fd397127582d65cf439920d577d52a4
Irina <uifjlllhfyy@.ru>
2020-03-26 14:53:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# speaker-child
+ Дочерняя тема для speaker-child
---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+add_action('wp_enqueue_scripts', 'my_theme_styles' );
+function my_theme_styles() {
+	wp_enqueue_style('parent-theme-css', get_template_directory_uri() .'/style.css' );
+	// не обязательно, правильная родительская тема подключит его сама.
+	//wp_enqueue_style('child-theme-css', get_stylesheet_directory_uri() .'/style.css', array('parent-theme-css') );
+}
+
+//test
