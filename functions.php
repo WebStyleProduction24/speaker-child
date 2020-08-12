431ecf730b307edeb519f041b4b45737af2a43ba
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2020-08-12 18:08:58 +0300

    Update 2020/08/12 18:08 +3GMT (MSK)
    Добавлен в блок контактов header e-mail (жестко через код)

--- functions.php
+++ functions.php
@@ -5,4 +5,70 @@ function theme_enqueue_styles(){
 }
 
 
+
+/**
+ * Show contact block.
+ *
+ * @since  1.0.0
+ * @param string $target Current block position: header, footer.
+ */
+function speaker_contact_block_wsp24( $target = 'header' ) {
+	$contact_block_visibility = get_theme_mod( $target . '_contact_block_visibility', speaker_theme()->customizer->get_default( $target . '_contact_block_visibility' ) );
+
+	if ( ! $contact_block_visibility ) {
+		return;
+	}
+
+	$contact_item_count = apply_filters( 'speaker_contact_item_count', array(
+		'header'           => 3,
+		'header_top_panel' => 4,
+		'footer'           => 3,
+	) );
+
+	$contact_info = array();
+
+	for ( $i = 1; $i <= $contact_item_count[ $target ]; $i ++ ) {
+		$icon  = get_theme_mod( $target . '_contact_icon_' . $i, speaker_theme()->customizer->get_default( $target . '_contact_icon_' . $i ) );
+		$label = get_theme_mod( $target . '_contact_label_' . $i, speaker_theme()->customizer->get_default( $target . '_contact_label_' . $i ) );
+		$value = get_theme_mod( $target . '_contact_text_' . $i, speaker_theme()->customizer->get_default( $target . '_contact_text_' . $i ) );
+		if ( ! $icon && ! $value && ! $label ) {
+			continue;
+		}
+		$contact_info [ 'item_' . $i ] = array(
+			'icon'  => $icon,
+			'label' => $label,
+			'value' => $value,
+		);
+	}
+
+	if ( ! $contact_info ) {
+		return;
+	}
+
+	$icon_format = apply_filters( 'speaker_contact_block_icon_format', '<i class="contact-block__icon nc-icon-mini %1$s"></i>' );
+
+	$html = '<div class="contact-block contact-block--' . $target . '"><div class="contact-block__inner">';
+
+	foreach ( $contact_info as $key => $value ) {
+		$icon           = ( $value['icon'] ) ? sprintf( $icon_format, $value['icon'] ) : '';
+		$label          = ( $value['label'] ) ? sprintf( '<span class="contact-block__label">%1$s</span>', wp_unslash( $value['label'] ) ) : '';
+		$text           = ( $value['value'] ) ? sprintf( '<span class="contact-block__text">%1$s</span>', wp_kses( wp_unslash( $value['value'] ), wp_kses_allowed_html( 'post' ) ) ) : '';
+		$item_mod_class = ( $value['icon'] ) ? 'contact-block__item--icon' : '';
+
+		$html .= sprintf( '<div class="contact-block__item %1$s">%2$s<div class="contact-block__value-wrap">%3$s%4$s</div></div>', $item_mod_class, $icon, $label, $text );
+	}
+
+	$email_column_wsp24 = '<div class="contact-block__item contact-block__item--icon"><i class="contact-block__icon nc-icon-mini ui-1_email-83"></i><div class="contact-block__value-wrap"><span class="contact-block__text"><a href="mailto:info@банкрот36.рф">info@банкрот36.рф</a></span></div>';
+
+	$html .= $email_column_wsp24 . '</div></div>';
+
+	echo $html;
+}
+
+
+
+
+
+
+
 ?>
