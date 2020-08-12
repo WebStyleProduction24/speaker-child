3845e466acff3b0b30599e8dd784ce2c6537a856
Irina <uifjlllhfyy@.ru>
2020-08-12 23:31:49 +0300

    12.08.2020

---
+++ header.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Speaker
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+	
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(66078253, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/66078253" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->	
+</head>
+
+<body <?php body_class(); ?>>
+<?php speaker_get_page_preloader(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'speaker' ); ?></a>
+	<header id="masthead" <?php speaker_header_class(); ?> role="banner">
+		<?php speaker_ads_header() ?>
+		<?php get_template_part( 'template-parts/header/mobile-panel' ); ?>
+		<?php get_template_part( 'template-parts/header/top-panel', get_theme_mod( 'header_layout_type', speaker_theme()->customizer->get_default( 'header_layout_type' ) ) ); ?>
+
+		<div <?php speaker_header_container_class(); ?>>
+			<?php get_template_part( 'template-parts/header/layout', get_theme_mod( 'header_layout_type', speaker_theme()->customizer->get_default( 'header_layout_type' ) ) ); ?>
+		</div><!-- .header-container -->
+	</header><!-- #masthead -->
+
+	<div id="content" <?php speaker_content_class(); ?>>
+
+		
+<!-- <?php if ( !is_home() || !is_front_page() ) { ?>
+
+<?php } ?> -->
